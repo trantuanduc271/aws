7f7015fa0ecfef41b7b7876093e3a703bca562b3
Tran Tuan Duc <tuanduc.tran@outlook.com>
2024-03-27 22:07:59 +0700

    Create VPC, VPC Routetable and VPC Subnet

--- locals.tf
+++ locals.tf
@@ -19,10 +19,10 @@ locals {
   internet_gateway_name    = "igw-${local.name_suffix}"
   nat_gateway_name         = "nat-${local.name_suffix}"
   eip_name                 = "eip-${local.name_suffix}"
-  public_route_table_name  = "rtb-${replace(local.name_suffix, var.customer, "public")}"
-  private_route_table_name = "rtb-${replace(local.name_suffix, var.customer, "private")}"
+  public_route_table_name  = "rtb-${replace(local.name_suffix, var.author, "public")}"
+  private_route_table_name = "rtb-${replace(local.name_suffix, var.author, "private")}"
   subnet_names = {
     for subnet_key, subnet in var.subnets : subnet_key =>
-    format("subnet-%s-%s%s", subnet.is_public ? "public" : "private", replace(local.name_suffix, "${var.customer}-", ""), subnet.zone)
+    format("subnet-%s-%s%s", subnet.is_public ? "public" : "private", replace(local.name_suffix, "${var.author}-", ""), subnet.zone)
   }
 }
